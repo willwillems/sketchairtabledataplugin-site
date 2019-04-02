d07faeaca454ed99df0848f11861cb83bd9daed5
Rutger Willems <rut.willems@gmail.com>
2019-04-02 15:05:52 +0800

    add navbar

---
+++ components/TheNavBar.vue
@@ -0,0 +1,30 @@
+<template lang="pug">
+  nav.nav-bar
+    .content-container.nav-bar__content
+      .nav__logo
+        img
+      .nav__links
+        a( href="" ) Features
+        a( href="" ) F.A.Q.
+        a( href="" )
+        button( click="" ) Buy
+</template>
+
+<style lang="scss" scoped>
+.nav-bar {
+  width: 100%;
+  height: 60px;
+  position: fixed;
+  top: 0;
+  display: flex;
+  justify-content: center;
+
+  &__content {
+    flex-grow: 1;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+  }
+}
+</style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,8 +1,20 @@
 <template>
   <div>
+    <TheNavBar />
     <nuxt />
   </div>
 </template>
+
+<script>
+import TheNavBar from '@/components/TheNavBar'
+
+export default {
+  components: {
+    TheNavBar
+  }
+}
+</script>
+
 
 <style>
 html {
@@ -24,32 +36,12 @@ html {
   margin: 0;
 }
 
-.button--green {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #3b8070;
-  color: #3b8070;
-  text-decoration: none;
-  padding: 10px 30px;
+.content-container {
+  max-width: 960px;
+  padding: 10px;
 }
 
-.button--green:hover {
-  color: #fff;
-  background-color: #3b8070;
-}
-
-.button--grey {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #35495e;
-  color: #35495e;
-  text-decoration: none;
-  padding: 10px 30px;
-  margin-left: 15px;
-}
-
-.button--grey:hover {
-  color: #fff;
-  background-color: #35495e;
+.section {
+  
 }
 </style>
