9fbfe58672edd06871558363abc8393d1a76c455
Rutger Willems <rut.willems@gmail.com>
2019-04-10 08:10:38 +0700

    add crude 404 page

---
+++ pages/404.vue
@@ -0,0 +1,26 @@
+<template lang="pug">
+  main
+    section.section.section-404
+      .content-container( style="text-align: center;" )
+          h1.title( style="font-size: 8rem;" ) 404
+          h2.sub-title  Page not found, get back <a href="/">home</a>.
+</template>
+
+<style lang="scss" scoped>
+.section-404 {
+  height: 70vh;
+}
+
+.title {
+  font-weight: 800;
+  font-size: 3.5rem;
+  line-height: 1em;
+  padding: 0.1em 0;
+}
+
+.sub-title {
+  margin: 12px 0;
+  letter-spacing: -1px;
+  color: rgb(87, 87, 87);
+}
+</style>
